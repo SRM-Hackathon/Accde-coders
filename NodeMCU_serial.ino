316baca82661e250d3186cdfe26cfa52953f9e56
Sukesh Seth <41232037+sukesh2000@users.noreply.github.com>
2019-10-12 05:23:52 +0530

    Add files via upload
    Arduino UNO - NodeMCU codes

---
+++ NodeMCU_serial.ino
@@ -0,0 +1,40 @@
+// Robo India Tutorial 
+// Digital Input and Output on LED 
+// Hardware: NodeMCU
+
+const int ARD1 = D2;
+const int LED = D7;
+const int ARD2 = D8;
+int ARDstate1 = 0;
+int ARDstate2 = 0;
+
+void setup(){ 
+  pinMode(LED, OUTPUT);
+  pinMode (ARD1, INPUT);
+  pinMode (ARD2, INPUT);
+  Serial.begin(9600);
+ }
+void loop() {
+  ARDstate1 = digitalRead(ARD1);
+  ARDstate2 = digitalRead(ARD2); 
+  if (ARDstate1 == LOW)
+    {
+      digitalWrite(D7, LOW);
+      //Serial.print("sensor: ");
+      //Serial.println(digitalRead(D7));
+    }
+    else
+    {
+      digitalWrite(D7, HIGH);
+    }
+    if (ARDstate2 == HIGH)
+    {
+     digitalWrite(D4, HIGH);
+     Serial.print("mpu: ");
+     Serial.println(digitalRead(ARDstate2));
+    }
+    else
+    {
+      digitalWrite(D4, LOW);
+    }
+}
